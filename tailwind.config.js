d8f07fab89d9e5c175ab876c73773fb728e8dc9a
sandys-ss <sandysyam@gmail.com>
2021-08-17 16:36:55 +0800

    landing page

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ["./*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#FFE8EE',
+      },
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
