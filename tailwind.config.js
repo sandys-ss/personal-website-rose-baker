06d62b2c1d235c3a5b95ac2e2427b056988d54db
sandys-ss <sandysyam@gmail.com>
2021-08-21 23:19:26 +0800

    responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ["./*.html"],
+  //purge: ["./*.html"],
+  purge: {
+    enabled: false,
+    content: ['./*.html'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
