6dee5d89f6f4588e5a0388bd2d57f08bec12fdf5
Sandys <sandysyam@gmail.com>
2021-08-29 16:38:39 +0800

    external javascript

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log('hello javascript');
+
+const btn = document.querySelector("button.btn-mobile");
+const menu = document.querySelector(".btn-menu");
+
+    btn.addEventListener("click", () => {
+      menu.classList.toggle("hidden");
+    } );
+
+    window.addEventListener("resize", () => {
+        if (window.innerWidth > 768) {
+            menu.classList.toggle("hidden");
+        };
+    });
