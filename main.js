1dc23aaef613b39f9fcd068ded20fa4070a00d9d
Sandys <sandysyam@gmail.com>
2021-09-10 16:48:26 +0800

    update main js

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
 import "tailwindcss/tailwind.css";
+
+console.log('hello javascript');
+
+//responsive
+const btn = document.querySelector("button.btn-mobile");
+const menu = document.querySelector(".btn-menu");
+
+    btn.addEventListener("click", () => {
+      menu.classList.toggle("hidden");
+    } );
+
+
+//Slideshow quote
+
+var slideIndex = 1;
+showSlides(slideIndex);
+    
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+    
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+    
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        
+        for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+        }
+
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
